00bf357f759bf01ebcb368522b03b3ce1ae4db91
Leikooo <solomon.www@mail.ru>
2022-12-03 19:22:14 +0300

    Completed code

---
+++ main.py
@@ -0,0 +1,14 @@
+import test
+from calculator import Calculator
+
+def main():
+    test.test_func()
+    a=int(input('Enter a first number: '))
+    b=int(input('Enter a second number: '))
+    calc=Calculator()
+    print(calc.add(a,b))
+
+if __name__ == "__main__":
+    main()
+
+
