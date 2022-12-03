e2224b105bdcbffabc0153abb29dd4d5dc62b1cf
Leikooo <solomon.www@mail.ru>
2022-12-03 21:06:17 +0300

    Fixed

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestCalculator(unittest.TestCase):
   def test_subtract(self):
     self.assertEqual(self.calculator.subtract(10,5), 5)
   def test_multiply(self):
-    self.assertEqual(self.calculator.multiply(3,7), 1)
+    self.assertEqual(self.calculator.multiply(3,7), 21)
   def test_divide(self):
     self.assertEqual(self.calculator.divide(10,2), 5)
 
